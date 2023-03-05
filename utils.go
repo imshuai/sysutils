d2bbf798d948deacac8fa06faea746276e2d15ff
im帥 <iris-me@live.com>
2023-03-05 22:48:16 +0800

    添加ExpireableStorage，可提供临时使用的可定期清理过期内容的存储

--- utils.go
+++ utils.go
@@ -1,30 +1,22 @@
 package sysutils
 
 import (
+	"fmt"
 	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"reflect"
 	"strings"
-    "time"
-    "fmt"
+	"time"
+
+	"github.com/robfig/cron/v3"
 )
 
 var (
-	separator string
-    SHANGHAI, _ = time.LoadLocation("Asia/Shanghai")
+	separator               string
+	TimeLocationShanghai, _ = time.LoadLocation("Asia/Shanghai")
 )
-
-//PathSeparator 返回系统路径分隔符
-func PathSeparator() string {
-	return separator
-}
-
-//PathJoin 判断系统路径分隔符并返回对应的路径字符串拼接结果
-func PathJoin(path ...string) string {
-	return strings.Join(path, separator)
-}
 
 func init() {
 	if os.IsPathSeparator('\\') { //前边的判断是否是系统的分隔符
@@ -34,7 +26,17 @@ func init() {
 	}
 }
 
-//GetCurPath 获取当前文件执行的路径
+// PathSeparator 返回系统路径分隔符
+func PathSeparator() string {
+	return separator
+}
+
+// PathJoin 判断系统路径分隔符并返回对应的路径字符串拼接结果
+func PathJoin(path ...string) string {
+	return strings.Join(path, separator)
+}
+
+// GetCurPath 获取当前文件执行的路径
 func GetCurPath() string {
 	file, _ := exec.LookPath(os.Args[0])
 
@@ -45,7 +47,7 @@ func GetCurPath() string {
 	return rst
 }
 
-//ObjectToURLValues 把json结构体转换为url.values
+// ObjectToURLValues 把json结构体转换为url.values
 func ObjectToURLValues(p interface{}) url.Values {
 	t := reflect.TypeOf(p)
 	v := reflect.ValueOf(p)
@@ -105,7 +107,7 @@ func (t Time) String() string {
 }
 
 func Now() Time {
-	return Time{time.Now().Local().In(SHANGHAI)}
+	return Time{time.Now().Local().In(TimeLocationShanghai)}
 }
 
 // 字节的单位转换 保留两位小数
@@ -124,4 +126,65 @@ func FormatSize(fileSize int64) (size string) {
 	} else { //if fileSize < (1024 * 1024 * 1024 * 1024 * 1024 * 1024)
 		return fmt.Sprintf("%.2fEB", float64(fileSize)/float64(1024*1024*1024*1024*1024))
 	}
+}
+
+type Expireable interface {
+	Key() string
+	IsExpired() bool
+}
+
+type ExpireableStorage struct {
+	data    map[string]Expireable
+	crontab *cron.Cron
+}
+
+func (s *ExpireableStorage) Add(delta Expireable) {
+	s.data[delta.Key()] = delta
+}
+
+func (s *ExpireableStorage) Del(delta Expireable) {
+	delete(s.data, delta.Key())
+}
+
+func (s *ExpireableStorage) Get(key string) (Expireable, error) {
+	if v, ok := s.data[key]; ok {
+		return v, nil
+	}
+	return nil, fmt.Errorf("invalid key")
+}
+
+func (s *ExpireableStorage) Destroy() {
+	for k := range s.data {
+		delete(s.data, k)
+	}
+	ctx := s.crontab.Stop()
+	<-ctx.Done()
+	s = nil
+}
+
+func (s *ExpireableStorage) Run() {
+	count := 0
+	for k, v := range s.data {
+		if v.IsExpired() {
+			delete(s.data, k)
+			count++
+		}
+	}
+}
+
+func NewExpireableStorage(spec string) (*ExpireableStorage, error) {
+	es := &ExpireableStorage{
+		data:    make(map[string]Expireable),
+		crontab: cron.New(cron.WithLocation(TimeLocationShanghai)),
+	}
+	_, err := cron.ParseStandard(spec)
+	if err != nil {
+		return nil, err
+	}
+	_, err = es.crontab.AddJob(spec, es)
+	if err != nil {
+		return nil, err
+	}
+	es.crontab.Start()
+	return es, nil
 }
